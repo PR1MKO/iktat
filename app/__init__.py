ae5657b3185c89061b0fb80aebffefc12a7efddc
Istvan Kiss <kiss.istvan3@med.unideb.hu>
2025-06-11 11:42:32 +0200

    Add User model with hashed password and role, initialize DB

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from .models import db
+from flask_migrate import Migrate
 
 def create_app():
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///forensic_cases.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    migrate = Migrate(app, db)  # <-- ADD THIS LINE
 
     @app.route("/")
     def hello():
