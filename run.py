bb5c7543c53ccdce6f6d0d06d46772d1722817b5
PR1MKO <PR1MKO@users.noreply.github.com>
2025-07-09 14:20:08 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
+import subprocess
 from app import create_app
+
+# Run safety sync before app starts
+subprocess.run(["python", "scripts/safe_sync.py"])
 
 app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
+
