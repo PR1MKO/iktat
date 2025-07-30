7ff7c90ecf6255c049cfae5910a64893b7203e14
Istvan Kiss <kiss.istvan3@med.unideb.hu>
2025-07-30 13:24:35 +0200

    ✅ Apply Codex update: toxi

---
+++ migrations/versions/f28b14d743a3_merge_heads.py
@@ -0,0 +1,24 @@
+"""Merge heads
+
+Revision ID: f28b14d743a3
+Revises: 8cdac474f9fd, d4597dfc1abc
+Create Date: 2025-07-30 13:23:48.593484
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f28b14d743a3'
+down_revision = ('8cdac474f9fd', 'd4597dfc1abc')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
