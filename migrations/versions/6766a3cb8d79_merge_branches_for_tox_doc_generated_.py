56a8df0831764c3688c1e408949fb008ff857d68
Istvan Kiss <kiss.istvan3@med.unideb.hu>
2025-07-31 12:51:22 +0200

    Ôťů Apply Codex update: toxi

---
+++ migrations/versions/6766a3cb8d79_merge_branches_for_tox_doc_generated_.py
@@ -0,0 +1,24 @@
+"""Merge branches for tox_doc_generated fields
+
+Revision ID: 6766a3cb8d79
+Revises: 39caa26b164c, a02ad79f6bbd
+Create Date: 2025-07-31 12:50:43.525991
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6766a3cb8d79'
+down_revision = ('39caa26b164c', 'a02ad79f6bbd')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
