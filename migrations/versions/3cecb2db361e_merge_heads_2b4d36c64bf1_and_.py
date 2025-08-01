0c05a206bd57f08a5970557975b93b176943903e
Istvan Kiss <kiss.istvan3@med.unideb.hu>
2025-08-01 08:10:47 +0200

    Ôťů Apply Codex update: toxi

---
+++ migrations/versions/3cecb2db361e_merge_heads_2b4d36c64bf1_and_.py
@@ -0,0 +1,24 @@
+"""🔀 Merge heads 2b4d36c64bf1 and 6766a3cb8d79
+
+Revision ID: 3cecb2db361e
+Revises: 2b4d36c64bf1, 6766a3cb8d79
+Create Date: 2025-08-01 08:10:06.839549
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3cecb2db361e'
+down_revision = ('2b4d36c64bf1', '6766a3cb8d79')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
