6230822768bf4ca15873bda1905dc131b0646755
Istvan Kiss <kiss.istvan3@med.unideb.hu>
2025-08-12 08:01:24 +0200

    Ôťů Apply Codex update: toxi

--- app/investigations/forms.py
+++ app/investigations/forms.py
@@ -1,12 +1,13 @@
 # app/investigations/forms.py
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField, DateField, TextAreaField
+from wtforms import StringField, SelectField, DateField, TextAreaField, SubmitField
 from flask_wtf.file import FileField
 from wtforms.validators import DataRequired, Optional
 
+
 class InvestigationForm(FlaskForm):
     subject_name = StringField('Név', validators=[DataRequired()])
-    maiden_name = StringField('Leánykori név', validators=[Optional()])  # ← add this
+    maiden_name = StringField('Leánykori név', validators=[Optional()])
     mother_name = StringField('Anyja neve', validators=[DataRequired()])
     birth_place = StringField('Születési hely', validators=[DataRequired()])
     birth_date = DateField('Születési idő', format='%Y-%m-%d', validators=[DataRequired()])
@@ -27,17 +28,20 @@ class InvestigationForm(FlaskForm):
     external_case_number = StringField('Külső ügyszám', validators=[Optional()])
     other_identifier = StringField('Egyéb azonosító', validators=[Optional()])
 
+    submit = SubmitField('Mentés')
+
     def validate(self, *args, **kwargs):
         valid = super().validate(*args, **kwargs)
         if not valid:
             return False
-        # require at least one of the two ids
+        # Legalább az egyik azonosító kötelező
         if not self.external_case_number.data and not self.other_identifier.data:
             msg = 'Legalább az egyik azonosítót meg kell adni.'
             self.external_case_number.errors.append(msg)
             self.other_identifier.errors.append(msg)
             return False
         return True
+
 
 class FileUploadForm(FlaskForm):
     category = SelectField(
@@ -46,6 +50,7 @@ class FileUploadForm(FlaskForm):
         validators=[DataRequired()],
     )
     file = FileField('Fájl', validators=[DataRequired()])
+
 
 class InvestigationNoteForm(FlaskForm):
     text = TextAreaField('Megjegyzés', validators=[DataRequired()])
