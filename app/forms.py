fd15c93b36d0068f0579d53676c363420ad74153
Istvan Kiss <kiss.istvan3@med.unideb.hu>
2025-08-15 12:40:26 +0200

    Ôťů Apply Codex update: toxi

--- app/forms.py
+++ app/forms.py
@@ -9,7 +9,7 @@ from wtforms.validators import DataRequired, Optional, Length
 
 class AdminUserForm(FlaskForm):
     username = StringField('Felhasználónév', validators=[DataRequired(), Length(max=64)])
-    screen_name = StringField('Megjelenítendő név', validators=[Optional(), Length(max=64
+    screen_name = StringField('Megjelenítendő név', validators=[Optional(), Length(max=64)])
     full_name = StringField('Teljes név', validators=[Optional(), Length(max=128)])
     password = PasswordField('Jelszó', validators=[Optional()])
     role = SelectField('Szerepkör', choices=[
